dc0b7731c15a9a14e36a6fa28d2027cb2648c95a
Adrian Navarrete <a.navarrete.uk@gmail.com>
2018-07-08 22:57:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = { beethoven:'symphony', mozart: 'concerto', chopin:'nocturne'} 
 
 function updatePlaylist(playlist, singer, song) {
@@ -7,5 +8,9 @@ function updatePlaylist(playlist, singer, song) {
 
 function removeFromPlaylist(playlist, singer) {
   delete playlist[singer] 
+=======
+var playlist = { beethoven:"symphony", mozart: "concerto", chopin:"nocturne"  } 
+function updatePlaylist  (playlist, bach, mass) {
+>>>>>>> ae213825167fc50b4e838da2f8fdba7d515d5174
   return playlist
 }
